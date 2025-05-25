986a3506fde85925e25a4d2ab0a2055c46379315
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-05-26 01:24:55 +0530

    Implemented Login API

--- src/main/java/com/example/spring_api/service/AuthService.java
+++ src/main/java/com/example/spring_api/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.example.spring_api.service;
 
+import com.example.spring_api.dto.LoginRequest;
 import com.example.spring_api.dto.RegisterRequest;
 import com.example.spring_api.model.User;
 import com.example.spring_api.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -27,4 +30,23 @@ public class AuthService {
 
         userRepository.save(user);
     }
+
+    public String login(LoginRequest loginRequest){
+
+        // Find user by email
+        Optional<User> userOptional = userRepository.findByEmail(loginRequest.getEmail());
+
+        if(userOptional.isEmpty()){
+            throw new RuntimeException("User not found");
+        }
+
+        // Get the user and compare password
+        User user = userOptional.get();
+        if(!user.getPassword().equals(loginRequest.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+        return "Login Successful !"
+    }
+
+
 }
