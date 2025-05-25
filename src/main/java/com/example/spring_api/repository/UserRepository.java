7e9bf6706fd3163d1be9a77d13eddfe620560e52
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-05-25 23:02:16 +0530

    Initial commit

---
+++ src/main/java/com/example/spring_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.spring_api.repository;
+
+import com.example.spring_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
